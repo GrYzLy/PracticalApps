ee539a79a46e1031e8505ec1788bcc8c6202ae64
GrYzLy <bartosz.rzemek@gmail.com>
2024-05-25 13:17:39 +0200

    Initial commit

---
+++ PracticalApps/NorthwindMVC5/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NorthwindMVC5.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Packt.Shared;
+
+namespace NorthwindMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IHttpClientFactory clientFactory;
+        private readonly Northwind db;
+
+
+        public HomeController(ILogger<HomeController> logger, Northwind injectedContext, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            db = injectedContext;
+            clientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Customers(string country)
+        {
+            string uri;
+            if(string.IsNullOrEmpty(country))
+            {
+                ViewData["Title"] = "All Customers Worldwide";
+                uri = "api/customers/";
+            }
+            else
+            {
+                ViewData["Title"] = "Customers in " + country;
+                uri = "api/customers/?country=" + country;
+            }
+
+            var client = clientFactory.CreateClient(
+                name: "NorthwindService");
+
+            var request = new HttpRequestMessage(
+                method: HttpMethod.Get, requestUri: uri);
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            var model = await response.Content.ReadFromJsonAsync<IEnumerable<Customer>>();
+
+            return View(model);
+        }
+    }
+}
+
